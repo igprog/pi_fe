5e25cec264fb9289db2984a97d383340edb64c2f
Igor Gašparović <dragan.stanojevic@3cad.hr>
2021-09-27 11:45:55 +0200

    Sidenav

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,43 +1,43 @@
 <template>
-  <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid">
-      <a class="navbar-brand" href="../"
-        ><img src="../assets/logo.png" class="logo" alt="program ishrane"
-      /></a>
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarSupportedContent"
-        aria-controls="navbarSupportedContent"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul class="navbar-nav ml-auto">
-          <li v-for="(x, idx) in menu_" :key="idx" class="nav-item">
-            <router-link :to="'/' + x.link" :title="x.title">
-              <span class="nav-link">
-                <icon :name="x.ico"></icon>
-                {{ x.title }}
-              </span>
-            </router-link>
-          </li>
-        </ul>
-        <!-- <form class="form-inline my-2 my-lg-0">
-          <input
-            class="form-control mr-sm-2"
-            type="search"
-            placeholder="Search"
-            aria-label="Search"
-          />
-          <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-        </form> -->
+  <div>
+    <nav class="navbar navbar-expand-lg fixed-top navbar-light bg-light shadow-sm" >
+      <div class="container-fluid">
+        <a class="navbar-brand" href="../"><img src="../assets/logo.png" class="logo" alt="program ishrane" /></a>
+        <button class="navbar-toggler" type="button" aria-label="Toggle navigation" @click="openNav()" >
+          <span class="navbar-toggler-icon">
+            <i class="fas fa-bars" style="font-size: 28px"></i>
+          </span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav ml-auto">
+            <li v-for="(x, idx) in menu_" :key="idx" class="nav-item">
+              <router-link :to="'/' + x.link" :title="x.title">
+                <span class="nav-link text-dark">
+                  <icon :name="x.ico"></icon>
+                  {{ x.title }}
+                </span>
+              </router-link>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+    <div id="mySidenav" class="sidenav text-uppercase">
+      <div>
+        <a href="javascript:void(0)" title="Izbornik" class="font-weight-bold text-light" @click="closeNav()">Menu</a>
+        <a href="javascript:void(0)" class="closebtn" @click="closeNav()" title="Zatvori" ><strong>&times;</strong></a>
+        <hr class="m-0 mx-4 mb-2 bg-light" />
+      </div>
+      <div v-for="(x, idx) in menu_" :key="idx" @click="closeNav()">
+        <router-link :to="'/' + x.link" :title="x.title">
+          <span class="nav-link">
+            <icon :name="x.ico"></icon>
+            {{ x.title }}
+          </span>
+        </router-link>
       </div>
     </div>
-  </nav>
+  </div>
 </template>
 
 <script>
@@ -59,6 +59,18 @@ export default {
   computed: {
     menu_: function () {
       return this.menu.filter((a) => a.isVisible);
+    },
+  },
+  methods: {
+    openNav() {
+      if (document.getElementById("mySidenav").style.width == "250px") {
+        document.getElementById("mySidenav").style.width = "0";
+      } else {
+        document.getElementById("mySidenav").style.width = "250px";
+      }
+    },
+    closeNav() {
+      document.getElementById("mySidenav").style.width = "0";
     },
   },
 };
